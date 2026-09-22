746dea3dac51fb4bbdb04375a509775833c6f8c0
ShaikhUzair7777 <shaikhuzair7777@gmail.com>
2026-09-22 22:38:19 +0530

    Restrict backend CORS to production frontend

--- backend/main.py
+++ backend/main.py
@@ -24,7 +24,9 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+   allow_origins=[
+    "https://ai-personalized-learning-recommenda.vercel.app",
+],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
